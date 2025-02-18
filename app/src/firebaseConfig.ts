6de49992cd6d7d5f9dc04ae7d78670f162309b34
alex <alexalarcon200@gmail.com>
2025-02-18 13:00:41 -0500

    secure firebaseConfig credentials

--- app/src/firebaseConfig.ts
+++ app/src/firebaseConfig.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
-
 const firebaseConfig = {
-apiKey: "AIzaSyBkN1teCggKcRgbB5-lWvai3T_oEYVugIU",
-authDomain: "react-e-commerce-app-54528.firebaseapp.com",
-projectId: "react-e-commerce-app-54528",
-storageBucket: "react-e-commerce-app-54528.firebasestorage.app",
-messagingSenderId: "525875212191",
-appId: "1:525875212191:web:d4744f504ac2c473ef57aa"
+apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
